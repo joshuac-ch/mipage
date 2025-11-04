8071aa35a1b7c31f1ea3dda7841d4e817a99672c
joshua <condorenajoshua98@gmail.com>
2025-11-03 20:01:06 -0500

    Se modifico el header UI y se agrego el cv

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,10 +11,12 @@ export default function Header() {
     <div className="">
          <ul className="flex flex-row gap-4">
             <li className="cursor-pointer hover:text-indigo-400/80 hover:scale-120 transition-all">
-                <Github className=""></Github>               
+                <a href="https://github.com/joshuac-ch" target={'_blank'}><Github className=""></Github> </a>              
             </li>
             <li className="cursor-pointer hover:text-indigo-400/80 hover:scale-120 transition-all">
+                <a href="https://www.linkedin.com/in/joshua-gustavo-condorena-chuquica%C3%B1a-57817228b/" target={'_blank'}>
                 <Linkedin></Linkedin>                
+                </a>
             </li>
         </ul>
     </div>
@@ -22,11 +24,11 @@ export default function Header() {
 
     <div>
         <ul className="hidden md:flex text-md flex-row gap-10">
-            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/">Sobre mi</a></li>
-            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/#experiencia">Experiencia</a></li>
-            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/proyectos/">Proyectos</a> </li>
-            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/#contactar">Contactar</a></li>
-            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/extras">Extras</a></li>
+            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/index.html">Sobre mi</a></li>
+            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/index.html#experiencia">Experiencia</a></li>
+            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/proyectos/index.html">Proyectos</a> </li>
+            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/index.html#contactar">Contactar</a></li>
+            <li className="transition-all duration-300 cursor-pointer hover:text-indigo-400/80"><a href="/extras/index.html">Extras</a></li>
         </ul>
     </div>
     <div className="">
@@ -44,11 +46,11 @@ export default function Header() {
      {
         open&&(
             <div className="md:hidden absolute top-16 flex flex-col bg-black/20 left-0 items-center p-6 gap-6 transition-all w-full">
-            <a href="/" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Sobre mi</a>
-            <a href="/#experiencia" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Experiencia</a>
-            <a href="/proyectos" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Proyectos</a>
-            <a href="/#contactar" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Contactar</a>
-            <a href="/extras" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Extras</a>
+            <a href="/index.html" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Sobre mi</a>
+            <a href="/index.html#experiencia" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Experiencia</a>
+            <a href="/proyectos/index.html" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Proyectos</a>
+            <a href="/index.html#contactar" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Contactar</a>
+            <a href="/extras/index.html" className="hover:text-indigo-400/80 transition-all" onClick={()=>setopen(false)}>Extras</a>
             </div>
         )
      }
